c7db586b3eac4fae1183d2db5117366b87199a91
oogway <mattiapapaccioli@gmail.com>
2025-11-13 14:41:37 +0100

    argfgggg

--- src/index.js
+++ src/index.js
@@ -1,29 +1,3 @@
-"use strict";
-var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
-};
-var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-  }
-  return to;
-};
-var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-
-// src/texasHoldEmOdds.ts
-var texasHoldEmOdds_exports = {};
-__export(texasHoldEmOdds_exports, {
-  default: () => texasHoldEmOdds_default
-});
-module.exports = __toCommonJS(texasHoldEmOdds_exports);
-
 // src/lib/constants.ts
 var RANKS = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"];
 var SUITS = ["s", "h", "c", "d"];
@@ -929,7 +903,7 @@ var calculateHandHandEquities = (handStrings, dealtBoard) => {
 };
 
 // src/texasHoldEmOdds.ts
-var texasHoldEmOdds = (hands, board) => {
+function texasHoldEmOdds(hands, board) {
   if (hands === void 0 || hands === null) {
     return JSON.stringify({ error: "u need to put hands" });
   }
@@ -970,5 +944,7 @@ var texasHoldEmOdds = (hands, board) => {
     };
   });
   return JSON.stringify(results, null, 2);
+}
+export {
+  texasHoldEmOdds
 };
-var texasHoldEmOdds_default = texasHoldEmOdds;
